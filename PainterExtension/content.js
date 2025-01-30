f9432c17b6174018707251e9c556961acd5d10c3
Oscar Ludwig <134669025+OscarLudwig@users.noreply.github.com>
2025-01-29 19:39:13 -0800

    basic extension that runs on canvas pages and says hello in console

---
+++ PainterExtension/content.js
@@ -0,0 +1,4 @@
+// Call sayHello to log hello
+// The initial general structure is calling a
+// function for each feature that exists in a seperate file
+sayHello();
---
+++ PainterExtension/features/sayHello.js
@@ -0,0 +1,4 @@
+// Temporary feature to say hello at start of website console to show extension is working
+function sayHello() {
+    console.log("Hello from Canvas Painter!");
+}
