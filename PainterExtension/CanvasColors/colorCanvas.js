ef599056df6535b77f2a1167062ad25b9cfef9c6
Jacob-Porter <jkp98199@gmail.com>
2025-02-02 11:16:20 -0800

    more additions - edits to js
    added try-catches in js
    couple more darkmode additions to cover more pages

--- PainterExtension/CanvasColors/colorCanvas.js
+++ PainterExtension/CanvasColors/colorCanvas.js
@@ -32,33 +32,89 @@ function enableDarkMode() {
     }
     document.body.classList.add('darkmode');
     
-    var mini_calendars = document.querySelectorAll('.day_wrapper');
-    mini_calendars.forEach(element => {
-        element.classList.add('darkmode');
-    });
+    try {
+        var mini_calendars = document.querySelectorAll('.day_wrapper');
+        mini_calendars.forEach(element => {
+            element.classList.add('darkmode');
+        });
+    } catch {
+        console.log('no ".day_wrapper" class found...');
+    }
 
-    var mini_calendars = document.querySelectorAll('#content_wrapper');
-    mini_calendars.forEach(element => {
-        element.classList.add('darkmode');
-    });
+    try {
+        var mini_calendars = document.querySelectorAll('#content_wrapper');
+        mini_calendars.forEach(element => {
+            element.classList.add('darkmode');
+        });
+    } catch {
+        console.log('no "#content_wrapper" id found...');
+    }
 
-    var role_link = document.querySelectorAll('#breadcrumbs');
-    role_link.forEach(element => {
-        element.style.backgroundColor = 'transparent';
-    });
-
-    var header_bar = document.querySelectorAll('.header-bar');
-    header_bar.forEach(element => {
-        element.style.backgroundColor = 'transparent';
-    });
-
-    var tr = document.querySelector('#syllabusTableBody').querySelectorAll("tr");
-    tr.forEach(element => {
-        var td = element.querySelectorAll("td");
-        td.forEach(element => {
+    try {
+        var role_link = document.querySelectorAll('#breadcrumbs');
+        role_link.forEach(element => {
             element.style.backgroundColor = 'transparent';
         });
-    });
+    } catch {
+        console.log('no "#breadcrumbs" id found...');
+    }
+
+    try {
+        var header_bar = document.querySelectorAll('.header-bar');
+        header_bar.forEach(element => {
+            element.style.backgroundColor = 'transparent';
+        });
+    } catch {
+        console.log('no ".header-bar" class found...');
+    }
+
+    try {
+        var tr = document.querySelector('#syllabusTableBody').querySelectorAll("tr");
+        tr.forEach(element => {
+            var td = element.querySelectorAll("td");
+            td.forEach(element => {
+                element.style.backgroundColor = 'transparent';
+            });
+        });
+    } catch {
+        console.log('no "#syllabusTableBody" id found...');
+    }
+
+    try {
+        var context_modules = document.querySelectorAll(".context_modules");
+        context_modules.forEach(element => {
+            var context_module = element.querySelectorAll(".context_module");
+            context_module.forEach(element => {
+                var headers = element.querySelectorAll('.ig-header');
+                headers.forEach(element => {
+                    element.style.backgroundColor = 'green';
+                });
+            });
+        });
+    } catch {
+        console.log('no ".context_modules" class found...');
+    }
+
+    try {
+        var headers = element.querySelectorAll('.header');
+        headers.forEach(element => {
+            element.style.backgroundColor = 'green';
+        });
+
+    } catch {
+        console.log('no ".header" class found...');
+    }
+
+    try {
+        var headers = element.querySelectorAll('.context_module_item');
+        headers.forEach(element => {
+            element.style.backgroundColor = 'blue';
+        });
+
+    } catch {
+        console.log('no ".context_module_item" class found...');
+    }
+    
 }
 
 // Remove dark mode CSS
@@ -68,21 +124,49 @@ function disableDarkMode() {
         darkModeStylesheet = null;
     }
     document.body.classList.remove('darkmode');
-    var mini_calendars = document.querySelectorAll('.day_wrapper');
-    mini_calendars.forEach(element => {
-        element.classList.remove('darkmode');
-    });
 
-    var mini_calendars = document.querySelectorAll('#content_wrapper');
-    mini_calendars.forEach(element => {
-        element.classList.remove('darkmode');
-    });
-
-    var tr = document.querySelector('#syllabusTableBody').querySelectorAll("tr");
-    tr.forEach(element => {
-        var td = element.querySelectorAll("td");
-        td.forEach(element => {
-            element.style.backgroundColor = '';
+    try {
+        var mini_calendars = document.querySelectorAll('.day_wrapper');
+        mini_calendars.forEach(element => {
+            element.classList.remove('darkmode');
         });
-    });
+    } catch {
+        console.log('no ".day_wrapper" class found...');
+    }
+
+    try {
+        var mini_calendars = document.querySelectorAll('#content_wrapper');
+        mini_calendars.forEach(element => {
+            element.classList.remove('darkmode');
+        });
+    } catch {
+        console.log('no "#content_wrapper" id found...');
+    }
+
+    try {
+        var tr = document.querySelector('#syllabusTableBody').querySelectorAll("tr");
+        tr.forEach(element => {
+            var td = element.querySelectorAll("td");
+            td.forEach(element => {
+                element.style.backgroundColor = '';
+            });
+        });
+    } catch {
+        console.log('no "#syllabusTableBody" id found...');
+    }
+
+    try {
+        var context_modules = document.querySelectorAll(".context_modules");
+        context_modules.forEach(element => {
+            var context_module = element.querySelectorAll(".context_module");
+            context_module.forEach(element => {
+                var headers = element.querySelectorAll('.ig-header');
+                headers.forEach(element => {
+                    element.style.backgroundColor = '';
+                });
+            });
+        });
+    } catch {
+        console.log('no ".context_modules" class found...');
+    }
 }
