0c5e7b17a3ab48f4661d23ae301d07c7b5e5cd55
Llarence <1010kai.t1010@gmail.com>
2025-02-07 18:33:45 -0800

    add gradebook reading

--- PainterExtension/GradeBook/grade_book.js
+++ PainterExtension/GradeBook/grade_book.js
@@ -1,10 +1,41 @@
+class Assignment {
+    constructor(title, grade, max) {
+        this.title = title
+        this.grade = grade
+        this.max = max
+    }
+
+    static fromHTML(elem) {
+        const gradeNodes = elem.getElementsByClassName("grade")[0].childNodes
+
+        return new Assignment(
+            elem.getElementsByClassName("title")[0]
+                .getElementsByTagName("a")[0],
+
+            gradeNodes.length == 5 ?
+                parseInt(gradeNodes[4].textContent)
+                : null,
+
+            parseInt(elem.getElementsByClassName("tooltip")[0].children[1]
+                .textContent.slice(1))
+        )
+    }
+}
+
+const assignments = []
+
 function setup() {
     console.log("Checking for gradebook")
     const grade_div = document.getElementById("grade-summary-content")
-    if (!grade_div) { return }
+    if (!grade_div) {
+        return
+    }
 
     const sidebar = document.getElementById("student-grades-right-content")
-    if (!sidebar) { return }
+    if (!sidebar) {
+        return
+    }
+
     console.log("Found gradebook")
 
     console.log("Reading data")
@@ -13,8 +44,9 @@ function setup() {
             continue;
         }
 
-        console.log(assignment.getElementsByClassName("title")[0].getElementsByTagName("a")[0])
+        assignments.push(Assignment.fromHTML(assignment))
     }
+
     console.log("Read data")
 }
 
