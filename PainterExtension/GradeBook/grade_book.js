782d37dcb88ea88b2dcd52cc4400692986178c9e
Llarence <1010kai.t1010@gmail.com>
2025-02-07 18:44:58 -0800

    add comments

--- PainterExtension/GradeBook/grade_book.js
+++ PainterExtension/GradeBook/grade_book.js
@@ -1,10 +1,15 @@
+// Holds the data associated with an assigment
+//  entry on the gradebook table
 class Assignment {
+    // A default contrusctor for a data class
     constructor(title, grade, max) {
         this.title = title
         this.grade = grade
         this.max = max
     }
 
+    // Gets the relevant data from a valid table entry (not hard_coded) in the
+    //  student_assignment table
     static fromHTML(elem) {
         const gradeNodes = elem.getElementsByClassName("grade")[0].childNodes
 
@@ -22,16 +27,22 @@ class Assignment {
     }
 }
 
+// Holds the found assignments
 const assignments = []
 
+// Sets up the code code by loading all the assignments
 function setup() {
+    // Checks that the page had a valid gradebook
     console.log("Checking for gradebook")
     const grade_div = document.getElementById("grade-summary-content")
+    // If not the script shoudln't run
     if (!grade_div) {
         return
     }
 
+    // Checks the sidebar is there
     const sidebar = document.getElementById("student-grades-right-content")
+    // If not the script shoudln't run
     if (!sidebar) {
         return
     }
@@ -39,11 +50,15 @@ function setup() {
     console.log("Found gradebook")
 
     console.log("Reading data")
+    // Goes through the table looking for the assignments
     for (const assignment of grade_div.getElementsByClassName("student_assignment")) {
+        // If a class is hard coded it is not a valid assignment it
+        //  it is something like the table bar or to the totals
         if (assignment.classList.contains("hard_coded")) {
             continue;
         }
 
+        // Adds the assignment
         assignments.push(Assignment.fromHTML(assignment))
     }
 
