9dc790ad6a385122dba0b551aa0af12911e58d08
Llarence <1010kai.t1010@gmail.com>
2025-02-17 17:09:20 -0800

    add tests

--- PainterExtension/Test/Javascript/grademath.test.js
+++ PainterExtension/Test/Javascript/grademath.test.js
@@ -19,3 +19,24 @@ test('Mean Test 3', () => {
         .toBeCloseTo(-23.68)
 })
 
+test('StdDev Test 1', () => {
+    expect(math.stdDev(sample1, false))
+        .toBeCloseTo(1)
+    expect(math.stdDev(sample1, true))
+        .toBeCloseTo(0.816496580927726)
+})
+
+test('StdDev Test 2', () => {
+    expect(math.stdDev(sample2, false))
+        .toBeCloseTo(3.272783388968954)
+    expect(math.stdDev(sample2, true))
+        .toBeCloseTo(3.104834939252005)
+})
+
+test('StdDev Test 3', () => {
+    expect(math.stdDev(sample3, false))
+        .toBeCloseTo(25.64943144269154)
+    expect(math.stdDev(sample3, true))
+        .toBeCloseTo(25.131207690837304)
+})
+
