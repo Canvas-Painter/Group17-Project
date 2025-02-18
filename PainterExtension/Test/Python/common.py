30569b9ee38091142cdcc88efb370c43414392a3
Llarence <1010kai.t1010@gmail.com>
2025-02-17 17:17:13 -0800

    move pytest to root path like Jest

--- PainterExtension/Test/Python/common.py
+++ PainterExtension/Test/Python/common.py
@@ -2,9 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import atexit
+from os.path import join, dirname
+
+extension_path = join(dirname(__file__), '../..')
 
 options = webdriver.ChromeOptions()
-options.add_argument(f'--load-extension=../..')
+options.add_argument(f'--load-extension={extension_path}')
 driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
 
 def clean():
