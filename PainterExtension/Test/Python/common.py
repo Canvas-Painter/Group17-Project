02b833685397244e0390c3c9e8c17a9f9e99517d
Llarence <1010kai.t1010@gmail.com>
2025-02-17 18:22:59 -0800

    add pyvirtualdisplay

--- PainterExtension/Test/Python/common.py
+++ PainterExtension/Test/Python/common.py
@@ -3,7 +3,10 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import atexit
 from os.path import join, dirname
+from pyvirtualdisplay import Display
 
+display = Display()
+display.start()
 extension_path = join(dirname(__file__), '../..')
 
 options = webdriver.ChromeOptions()
@@ -12,5 +15,6 @@ driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager()
 
 def clean():
     driver.close()
+    display.stop()
 
 atexit.register(clean)
