19a76678ff8714c67bc51cef907e61f04ca43cd3
Jacob-Porter <jkp98199@gmail.com>
2025-02-22 11:56:02 -0800

    custom theme progress
    now editing css vars with JS for darkmode - working on creating and applying custom themes

--- PainterExtension/addressbarPopup/popup.js
+++ PainterExtension/addressbarPopup/popup.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const darkmodeButton = document.getElementById("darkmode-btn");
+    const themeButton = document.getElementById("applyTheme-btn");
 
     if (!darkmodeButton) {
         console.error("Dark mode button not found in the document");
@@ -7,8 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Load saved state
-    chrome.storage.sync.get(['darkMode'], function(result) {
-        darkmodeButton.checked = result.darkMode || false;
+    chrome.storage.sync.get(['theme'], function(result) {
+        darkmodeButton.checked = result.theme || result.darkMode || false;
     });
 
     darkmodeButton.addEventListener('change', function() {
@@ -16,11 +17,28 @@ document.addEventListener("DOMContentLoaded", function () {
         
         // Save state
         chrome.storage.sync.set({ darkMode: isEnabled });
+        chrome.storage.sync.set({ theme: false });
         
         // Send message to colorCanvas.js
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {
                 action: 'toggleDarkMode',
+                enabled: isEnabled
+            });
+        });
+    });
+
+    themeButton.addEventListener('change', function() {
+        const isEnabled = themeButton.checked;
+        
+        // Save state
+        chrome.storage.sync.set({ theme: isEnabled });
+        chrome.storage.sync.set({ darkMode: false });
+        
+        // Send message to colorCanvas.js
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                action: 'toggleTheme',
                 enabled: isEnabled
             });
         });
