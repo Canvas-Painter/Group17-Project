a467cf8cdddeb453856bcfcfaedb5254e661aa1e
Jacob-Porter <jkp98199@gmail.com>
2025-02-23 11:23:01 -0800

    good progress on custom themes
    minor bug changes to follow...

--- PainterExtension/CanvasColors/colorCanvas.js
+++ PainterExtension/CanvasColors/colorCanvas.js
@@ -1,140 +1,55 @@
 let customStylesheet = null;
-const darkmode = {  name: 'darkmode',
-                    '--bg-0': '#383838',
-                    '--bg-1': '#4c4c4c',
-                    '--bg-2': '#transparent',
-                    '--text-0': '#e7e7e7',
-                    '--text-1': '#f8bc80',
-                    '--text-2': '#f8a451',
-                    '--sidebar': '#D73F09',
-                    '--links': '#f58c02',
-                    '--hamburger': '#f58c02',
-                    '--invrt': '1'
-};
-
-// Function to check if darkmode is already in storage
-function isDarkModeInStorage() {
-    console.log('isDarkModeInStorage() called');
-    return new Promise((resolve) => {
-        chrome.storage.sync.get(['darkmode'], (result) => {
-            resolve(result.darkmode !== undefined);
-        });
-    });
-}
-
-// Function to set darkmode in storage if it's not already there
-async function initializeDarkModeInStorage() {
-    console.log('initializeDarkModeInStorage() called');
-    const alreadyExists = await isDarkModeInStorage();
-    if (!alreadyExists) {
-        chrome.storage.sync.set({ darkmode: darkmode }, () => {
-            console.log('Darkmode initialized in storage.');
-        });
-    } else {
-        console.log('Darkmode already exists in storage.');
-    }
-}
-
-console.log('checking storage');
-chrome.storage.sync.get(null, function(items) {
-    console.log(items);
-});
-console.log('~~~ DONE ~~~');
-
-// Initialize darkmode in storage on script load
-initializeDarkModeInStorage();
-
 
 // Check stored state on load
-chrome.storage.sync.get(['theme'], function(result) {
-    if (result.theme) {
-        enableTheme(theme);
+chrome.storage.sync.get(['current_theme'], function(result) {
+    console.log('theme:', result.current_theme);
+    // update theme if current_theme is stored
+    if (result.current_theme !== null) {
+        updateTheme(true, result.current_theme);
     }
 });
-
-// Get the theme from localstorage
-function getTheme() {
-    return new Promise((resolve) => {
-        chrome.storage.sync.get(['theme'], (result) => {
-            resolve(result.theme);
-        });
-    });
-}
 
 // Listen for messages from address bar popup
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'toggleDarkMode') {
-        if (request.enabled) {
-            console.log('Darkmode Theme');
-            enableTheme(darkmode);
-        } else {
-            disableTheme();
-        }
-        console.log('ColorCanvas: DarkMode is now:', request.enabled ? 'enabled' : 'disabled');
-    } else if (request.action === 'toggleTheme') {
-        if (request.enabled) {
-            console.log('Custom Theme');
-            enableTheme(getTheme());
-        } else {
-            disableTheme();
-        }
-        console.log('ColorCanvas: Theme is now:', request.enabled ? 'enabled' : 'disabled');
+    if (request.action === 'toggleTheme') {
+        updateTheme(request.enabled, request.theme)
+        //console.log('ColorCanvas: DarkMode is now:', request.enabled ? 'enabled' : 'disabled');
+        sendResponse({response: 'Theme updated', status: 'success'});
     }
 });
 
 // Enable dark mode CSS with custom.css
-function enableTheme(theme) {
-    if (!customStylesheet) {
-        customStylesheet = document.createElement('link');
-        customStylesheet.rel = 'stylesheet';
-        customStylesheet.type = 'text/css';
-        customStylesheet.href = chrome.runtime.getURL('CanvasColors/custom.css');
-        customStylesheet.classList.add('theme');
-        document.head.appendChild(customStylesheet);
+function updateTheme(active, theme) {
+    if (active) {
+        // Create custom stylesheet if it doesn't exist
+        if (!customStylesheet) {
+            customStylesheet = document.createElement('link');
+            customStylesheet.rel = 'stylesheet';
+            customStylesheet.type = 'text/css';
+            customStylesheet.href = chrome.runtime.getURL('CanvasColors/custom.css');
+            customStylesheet.classList.add('theme');
+            document.head.appendChild(customStylesheet);
+        }
+
+        // update CSS variables
+        const root = document.querySelector(':root');
+        root.style.setProperty('--bg-0', theme.cssVars['--bg-0']);
+        root.style.setProperty('--bg-1', theme.cssVars['--bg-1']);
+        root.style.setProperty('--bg-2', theme.cssVars['--bg-2']);
+        root.style.setProperty('--text-0', theme.cssVars['--text-0']);
+        root.style.setProperty('--text-1', theme.cssVars['--text-1']);
+        root.style.setProperty('--text-2', theme.cssVars['--text-2']);
+        root.style.setProperty('--sidebar', theme.cssVars['--sidebar']);
+        root.style.setProperty('--links', theme.cssVars['--links']);
+        root.style.setProperty('--hamburger', theme.cssVars['--hamburger']);
+        root.style.setProperty('--invrt', theme.cssVars['--invrt']);
+        
+        // add class
+        document.body.classList.add('theme');
     }
-    console.log('enableTheme:', theme);
-
-    // // Get the stylesheet
-    // let stylesheet = null;
-    // for (let i = 0; i < document.styleSheets.length; i++) {
-    //     const sheet = document.styleSheets[i];
-    //     console.log('sheet:', sheet);
-    //     if (sheet.ownerNode && sheet.ownerNode.classList.contains('theme')) {
-    //         stylesheet = sheet;
-    //         break;
-    //     }
-    // }
-
-    // console.log('stylesheet:', stylesheet);
-
-    // Update CSS variables
-    // if (stylesheet) {
-    const root = document.querySelector(':root');
-    console.log('root:', getComputedStyle(root));
-    console.log('--bg-0:', root.style.getPropertyValue('--bg-0'));
-
-
-    root.style.setProperty('--bg-0', theme['--bg-0']);
-    root.style.setProperty('--bg-1', theme['--bg-1']);
-    root.style.setProperty('--bg-2', theme['--bg-2']);
-    root.style.setProperty('--text-0', theme['--text-0']);
-    root.style.setProperty('--text-1', theme['--text-1']);
-    root.style.setProperty('--text-2', theme['--text-2']);
-    root.style.setProperty('--sidebar', theme['--sidebar']);
-    root.style.setProperty('--links', theme['--links']);
-    root.style.setProperty('--hamburger', theme['--hamburger']);
-    root.style.setProperty('--invrt', theme['--invrt']);
-    // }
-    
-    document.body.classList.add('theme');
+    else {
+        // remove class
+        document.body.classList.remove('theme');
+    }
 }
 
-// Remove dark mode CSS
-function disableTheme() {
-    if (customStylesheet) {
-        customStylesheet.remove();
-        customStylesheet = null;
-    }
-    document.body.classList.remove('theme');
-
-}
--- PainterExtension/addressbarPopup/popup.js
+++ PainterExtension/addressbarPopup/popup.js
@@ -1,34 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const darkmodeButton = document.getElementById("darkmode-btn");
     const themeButton = document.getElementById("applyTheme-btn");
+    const saveButton = document.getElementById("saveTheme-btn");
+    const deleteButton = document.getElementById("deleteTheme-btn");
+    const editButton = document.getElementById("editTheme-btn");
+    const themeDropdown = document.getElementById("existingThemes");
 
-    if (!darkmodeButton) {
-        console.error("Dark mode button not found in the document");
-        return;
-    }
-
-    // Load saved state
-    chrome.storage.sync.get(['theme'], function(result) {
-        darkmodeButton.checked = result.theme || result.darkMode || false;
-    });
-
-    darkmodeButton.addEventListener('change', function() {
-        const isEnabled = darkmodeButton.checked;
-        
-        // Save state
-        chrome.storage.sync.set({ darkMode: isEnabled });
-        chrome.storage.sync.set({ theme: false });
-        
-        // Send message to colorCanvas.js
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {
-                action: 'toggleDarkMode',
-                enabled: isEnabled
-            });
-        });
-    });
-
-    /*
+    // Input fields for new theme values
     const themeNameInput = document.getElementById("themeName");
     const bg0Input = document.getElementById("--bg-0");
     const bg1Input = document.getElementById("--bg-1");
@@ -36,26 +14,264 @@ document.addEventListener("DOMContentLoaded", function () {
     const text0Input = document.getElementById("--text-0");
     const text1Input = document.getElementById("--text-1");
     const text2Input = document.getElementById("--text-2");
-    const text3Input = document.getElementById("--text-3");
     const sidebarInput = document.getElementById("--sidebar");
     const linksInput = document.getElementById("--links");
     const hamburgerInput = document.getElementById("--hamburger");
     const invrtInput = document.getElementById("--invrt");
-    */
 
-    themeButton.addEventListener('change', function() {
-        const isEnabled = themeButton.checked;
-        
-        // Save state
-        chrome.storage.sync.set({ theme: isEnabled });
-        chrome.storage.sync.set({ darkMode: false });
-        
-        // Send message to colorCanvas.js
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {
-                action: 'toggleTheme',
-                enabled: isEnabled
+    // darkmode
+    const darkmode = {  name: 'darkmode',
+                        cssVars: {
+                            '--bg-0': '#383838',
+                            '--bg-1': '#4c4c4c',
+                            '--bg-2': '#transparent',
+                            '--text-0': '#e7e7e7',
+                            '--text-1': '#f8bc80',
+                            '--text-2': '#f8a451',
+                            '--sidebar': '#D73F09',
+                            '--links': '#f58c02',
+                            '--hamburger': '#f58c02',
+                            '--invrt': '1'
+                        }
+};
+
+    // On load, if a current_theme object doesn't exist, create one
+    chrome.storage.sync.get('current_theme', function(result) {
+        if (!result.current_theme || typeof result.current_theme !== "object") {
+            chrome.storage.sync.set({ current_theme : null }, function() {
+                console.log("Empty 'current_theme' obj created");
+            });
+        }
+        chrome.storage.sync.get('current_theme', function(result) {
+            if (result.current_theme !== null) {
+                if (result.current_theme.name === 'darkmode') {
+                    darkmodeButton.checked = true;
+                    themeButton.disabled = true;
+                } else {
+                    themeButton.checked = true;
+                    darkmodeButton.disabled = true;
+                }
+            }
+        });
+    });
+
+    // On load, if a custom_themes object doesn't exist, create one
+    chrome.storage.sync.get('custom_themes', function(result) {
+        if (!result.custom_themes || typeof result.custom_themes !== "object") {
+            chrome.storage.sync.set({ custom_themes: [] }, function() {
+                console.log("Empty 'custom_themes' obj created");
+            });
+        }
+    });
+
+    function updateDropdown() {
+        console.log('updateDropdown');
+        chrome.storage.sync.get('custom_themes', function(result) {
+            themeDropdown.innerHTML = ""; // clear dropdown
+            const arr = result.custom_themes; // get themes array
+            arr.forEach(theme => {
+                const option = document.createElement("option");
+                option.value = theme.name;
+                option.text = theme.name;
+                themeDropdown.appendChild(option);
             });
         });
+    }
+
+    updateDropdown();
+
+    function updateCurrentTheme(enabled, theme) {
+        if (enabled) {
+            chrome.storage.sync.set({current_theme : theme}, function() {
+                console.log('updated current_theme:', theme);
+            });
+        } else {
+            chrome.storage.sync.set({current_theme : null}, function() {
+                console.log('updated current_theme:', null);
+            });
+        }
+    }
+
+    // Darkmode toggle
+    darkmodeButton.addEventListener('change', function() {    
+        const isEnabled = darkmodeButton.checked;
+        console.log('darkmodeButton clicked');
+
+        // themeButton on/off
+        themeButton.disabled = isEnabled;
+
+        // Send message to colorCanvas.js
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                action: 'toggleTheme',
+                enabled: isEnabled,
+                theme: darkmode
+            });
+        });
+
+        // update current_theme
+        updateCurrentTheme(isEnabled, darkmode);
+    });
+
+    // Theme toggle
+    themeButton.addEventListener('change', function() {
+        const isEnabled = themeButton.checked;
+        console.log('themeButton clicked');
+
+        // darkmodeButton on/off
+        darkmodeButton.disabled = isEnabled;
+
+        chrome.storage.sync.get('custom_themes', function(result) {
+            const arr = result.custom_themes;
+            // get selected theme from dropdown 
+            const theme = arr.find(theme => theme.name === themeDropdown.value);
+            console.log('selected theme:', theme);
+            if (theme != undefined) {
+                // Send message to colorCanvas.js
+                chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, {
+                        action: 'toggleTheme',
+                        enabled: isEnabled,
+                        theme: theme
+                    });
+                });
+            }
+
+           // update current_theme
+           updateCurrentTheme(isEnabled, theme);
+        });
+    });
+
+
+    // Save new theme from popup inputs as the "current" theme
+    saveButton.addEventListener('click', function() {
+        console.log('saveButton clicked');
+        // Retrieve values from input fields
+        const themeName = themeNameInput.value.trim();
+        const newTheme = {
+            name: themeName,
+            cssVars: {
+                '--bg-0': bg0Input.value.trim(),
+                '--bg-1': bg1Input.value.trim(),
+                '--bg-2': bg2Input.value.trim(),
+                '--text-0': text0Input.value.trim(),
+                '--text-1': text1Input.value.trim(),
+                '--text-2': text2Input.value.trim(),
+                '--sidebar': sidebarInput.value.trim(),
+                '--links': linksInput.value.trim(),
+                '--hamburger': hamburgerInput.value.trim(),
+                '--invrt': invrtInput.value.trim()
+            }
+        };
+
+        // Save the new theme as the current theme in storage
+        chrome.storage.sync.get('custom_themes', function(result) {
+            let arr = result.custom_themes;
+            arr.push(newTheme);
+            chrome.storage.sync.set({ custom_themes: arr }, function() {
+                console.log("Custom themes saved:", newTheme, '\n', arr);
+            });
+            // Set newly saved theme automatically...
+            chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                themeButton.checked = true;
+                darkmodeButton.checked = false;
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    action: 'toggleTheme',
+                    enabled: themeButton.checked,
+                    theme: newTheme
+                });
+            });
+
+            // update current_theme
+            updateCurrentTheme(true, newTheme);
+
+            // darkmodeButton off
+            darkmodeButton.disabled = themeButton.checked;
+        });
+    });
+
+    // Delete a custom theme
+    deleteButton.addEventListener('click', function() {
+        console.log('deleteButton clicked');
+        const to_delete = editButton.value;
+        chrome.storage.sync.get('custom_themes', function(result) {
+            let arr = result.custom_themes; // get themes array
+
+            chrome.storage.sync.get('current_theme', function(result) {
+                let current = result.current_theme; // get current theme
+                if (current.name === to_delete) {
+
+                    // alert colorCanvas.js to update the theme
+                    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                        themeButton.checked = false;
+                        chrome.tabs.sendMessage(tabs[0].id, {
+                            action: 'toggleTheme',
+                            enabled: themeButton.checked
+                        });
+                    });
+
+                    // set current_theme to null
+                    updateCurrentTheme(isEnabled, null);
+                }
+                let index = arr.findIndex(theme => theme.name === to_delete);
+                arr.splice(index, 1);
+                chrome.storage.sync.set({ custom_themes: arr }, function() {
+                    console.log("Custom themes deleted:", to_delete, '\n', arr);
+                });
+            });
+        });
+    });
+
+    // Edit a custom theme
+    editButton.addEventListener('click', function() {
+        console.log('editButton clicked');
+        const to_edit = editButton.value;
+        chrome.storage.sync.get('custom_themes', function(result) {
+            let arr = result.custom_themes; // get themes array
+            let index = arr.findIndex(theme => theme.name === to_edit);
+            let theme = arr[index];
+            console.log('editing:', theme);
+
+            // Set input fields to theme values
+            const newTheme = {
+                name: theme.name,
+                cssVars: {
+                    '--bg-0': bg0Input.value.trim(),
+                    '--bg-1': bg1Input.value.trim(),
+                    '--bg-2': bg2Input.value.trim(),
+                    '--text-0': text0Input.value.trim(),
+                    '--text-1': text1Input.value.trim(),
+                    '--text-2': text2Input.value.trim(),
+                    '--sidebar': sidebarInput.value.trim(),
+                    '--links': linksInput.value.trim(),
+                    '--hamburger': hamburgerInput.value.trim(),
+                    '--invrt': invrtInput.value.trim()
+                }
+            };
+
+            // Replace the old theme with the new theme in the array
+            arr[index] = newTheme;
+
+            // alert colorCanvas.js to update the theme
+            chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                themeButton.checked = true;
+                darkmodeButton.checked = false;
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    action: 'toggleTheme',
+                    enabled: themeButton.checked,
+                    theme: newTheme
+                });
+            });
+
+            // update current_theme
+            updateCurrentTheme(isEnabled, newTheme);
+            
+            // darkmodeButton on/off
+            darkmodeButton.disabled = isEnabled;
+        });
+    });
+
+    themeDropdown.addEventListener('click', function() {
+        updateDropdown();
     });
 });
