1ef71d6becd4618140d5179c9ec9be2ef7bee2fc
Llarence <1010kai.t1010@gmail.com>
2025-03-09 19:07:56 -0700

    comments

--- PainterExtension/Test/Python/test_grades.py
+++ PainterExtension/Test/Python/test_grades.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
-from common import driver, extension_path
+from common import driver, sample_pages
 from os import path
 from time import sleep
 
+# Sample calculations
 test1 = [
     ('Discussions', '10', '97.1', '9.83'),
     ('Discussions', '10', '97.2', '10.33 Likely impossible'),
@@ -12,6 +13,8 @@ test1 = [
     ('Quizzes', '50', '94', '3.42'),
     ('Exam', '50', '94', '26.83')
 ]
+
+# Sample calculations
 test2 = [
     ('Final Exam', '100', '98', '102.35 Likely impossible'),
     ('Labs', '100', '80', '-72.27 Likely impossible'),
@@ -19,12 +22,16 @@ test2 = [
     ('Get Ready\'s', '70', '96', '10.97'),
 ]
 
+
+# Inputs a series of tests to the grade calculator
 def run_target_grades(driver, tests):
+    # The inputs things
     dropdown = Select(driver.find_element(By.ID, 'test-dropdown'))
     points = driver.find_element(By.ID, 'test-points')
     grade = driver.find_element(By.ID, 'test-grade')
     output = driver.find_element(By.ID, 'test-output')
 
+    # Loops through and inputs the tests
     for drop, point, target, out in tests:
         dropdown.select_by_value(drop)
 
@@ -38,26 +45,32 @@ def run_target_grades(driver, tests):
 
 
 def test_grades1():
-    driver.get('file://' + path.join(extension_path, 'Test/Python/samples/grades1.html'))
+    driver.get(sample_pages[0])
+    # Gives time to load
     sleep(2)
 
+    # Check html loaded
     assert len(driver.find_elements(By.XPATH, '//*[@id="student-grades-right-content"]/div[7]')) > 0
     assert len(driver.find_elements(By.XPATH, '//*[@id="student-grades-right-content"]/div[8]')) > 0
     assert len(driver.find_elements(By.XPATH, '//*[@id="student-grades-right-content"]/div[9]')) > 0
     assert len(driver.find_elements(By.XPATH, '//*[@id="student-grades-right-content"]/div[10]')) > 0
     assert len(driver.find_elements(By.XPATH, '//*[@id="student-grades-right-content"]/div[11]')) > 0
 
+    # Run grade tests
     run_target_grades(driver, test1)
 
 
 def test_grades2():
-    driver.get('file://' + path.join(extension_path, 'Test/Python/samples/grades2.html'))
+    driver.get(sample_pages[1])
+    # Gives time to load
     sleep(2)
 
+    # Check html loaded
     assert len(driver.find_elements(By.XPATH, '//*[@id="student-grades-right-content"]/div[7]')) > 0
     assert len(driver.find_elements(By.XPATH, '//*[@id="student-grades-right-content"]/div[8]')) > 0
     assert len(driver.find_elements(By.XPATH, '//*[@id="student-grades-right-content"]/div[9]')) > 0
     assert len(driver.find_elements(By.XPATH, '//*[@id="student-grades-right-content"]/div[10]')) > 0
     assert len(driver.find_elements(By.XPATH, '//*[@id="student-grades-right-content"]/div[11]')) > 0
 
+    # Run grade tests
     run_target_grades(driver, test2)
--- PainterExtension/Test/Python/test_syllabus.py
+++ PainterExtension/Test/Python/test_syllabus.py
@@ -3,91 +3,119 @@ from selenium.webdriver.common.keys import Keys
 
 from common import driver, sample_pages
 
-
+# Gets the first child of an element
 def get_child(ele):
     return ele.find_element(By.XPATH, './*')
 
 
+# Edits the html replacing the first entry with first_text and first_value_text
 def check_edit(driver, first_path, first_value_path, first_text, first_value_text):
+    # Goes into edit mode
     driver.find_element(By.CLASS_NAME, 'edit-mode-btn').click()
 
+    # Replace the key
     first = driver.find_element(By.XPATH, first_path)
     first.click()
     child = get_child(first)
     child.send_keys(Keys.CONTROL + 'a' + Keys.DELETE)
     child.send_keys(first_text + Keys.ENTER)
 
+    # Replace the value
     firstValue = driver.find_element(By.XPATH, first_value_path)
     firstValue.click()
     get_child(firstValue).send_keys(first_value_text + Keys.ENTER)
 
+    # Check they were changed
     first = driver.find_element(By.XPATH, first_path)
     assert first.text == first_text
     firstValue = driver.find_element(By.XPATH, first_value_path)
     assert firstValue.text == first_value_text
 
 
+# Checks that cancel restored the html to the start state
 def check_cancel(driver, first_path, first_value_path):
+    # Cancels edit mode
     driver.find_element(By.CLASS_NAME, 'cancel-btn').click()
 
+    # Checks the values are at the same at the start
     first = driver.find_element(By.XPATH, first_path)
     assert first.text == 'Course Title'
     firstValue = driver.find_element(By.XPATH, first_value_path)
     assert firstValue.text == ''
 
 
+# Clicks the first delete button and checks the next item is correct
 def check_delete(driver, first_path, first_delete_path):
+    # Goes into edit mode
     driver.find_element(By.CLASS_NAME, 'edit-mode-btn').click()
 
+    # Clicks delete
     driver.find_element(By.XPATH, first_delete_path).click()
 
+    # Checks the delete was successful
     first = driver.find_element(By.XPATH, first_path)
     assert first.text == 'Professor'
 
 
+# Checks that an edit has successfully completed
 def check_done_edit(driver, first_path, first_value_path, first_text, first_value_text):
+    # Clicks the done button to confirm
     driver.find_element(By.CLASS_NAME, 'done-btn').click()
 
+    # Checks the first element key is changed
     first = driver.find_element(By.XPATH, first_path)
     assert first.text == first_text
 
+    # Checks the first element value is changed
     first = driver.find_element(By.XPATH, first_value_path)
     assert first.text == first_value_text
 
 
+# Checks that a delete edit was successful
 def check_done_delete(driver, first_path):
+    # Clicks the done button
     driver.find_element(By.CLASS_NAME, 'done-btn').click()
 
+    # Checks that the entry was deleted
     first = driver.find_element(By.XPATH, first_path)
     assert first.text == 'Professor'
 
 
 def test_edit_syllabus():
+    # Loads page
     driver.get(sample_pages[0])
 
     driver.find_element(By.CLASS_NAME, 'standard-syllabus').click()
 
+    # Switches to the iframe that actually does the work
     frame = driver.find_element(By.CLASS_NAME, 'syllabus-iframe')
     driver.switch_to.frame(frame)
 
+    # Checks that headers were loaded right
     headers = set([ele.text for ele in driver.find_elements(By.CLASS_NAME, 'category-header')])
     assert headers == set(['Course Information', 'Course Policies', 'Grading'])
 
+    # Loads the paths that are going to be checked
     first_path = '/html/body/main/div[2]/section[1]/div[2]/div/div[1]/div[2]'
     first_value_path = '/html/body/main/div[2]/section[1]/div[2]/div/div[1]/div[3]'
     first_delete_path = '/html/body/main/div[2]/section[1]/div[2]/div/div[1]/div[1]/button[2]'
 
+    # Sample texts to be inputted
     first_text = 'Not Course Title'
     first_value_text = 'Not A Course'
 
+    # Checks an edit then cancel
     check_edit(driver, first_path, first_value_path, first_text, first_value_text)
     check_cancel(driver, first_path, first_value_path)
 
+    # Checks a delete then cancel
     check_delete(driver, first_path, first_delete_path)
     check_cancel(driver, first_path, first_value_path)
 
+    # Checks an edit and confirm
     check_edit(driver, first_path, first_value_path, first_text, first_value_text)
     check_done_edit(driver, first_path, first_value_path, first_text, first_value_text)
 
+    # Checks a delete and confirm
     check_delete(driver, first_path, first_delete_path)
     check_done_delete(driver, first_path)
